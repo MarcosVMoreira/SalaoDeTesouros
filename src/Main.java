b35f042b252fc01f27aa7d1a2e8589e75daa08e0
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2019-04-06 22:48:41 -0300

    Add: empilhando resultados e finalizando o sistema
    Aprovado pelo run.codes mesmo sem podas.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
@@ -17,14 +18,14 @@ class Main {
 } 
 
 class EstadoCalculadora {
-    
+        
     private int c1, c2, objetivoC2 = 0, numeroOperacoes = 0;
     
     private Scanner scanner = new Scanner(System.in);
     
     private ArrayList<No> nivelAtual = new ArrayList<No>();
 
-    LinkedHashSet<String> jaUsados = new LinkedHashSet<>();
+    LinkedHashSet<String> jaUsados = new LinkedHashSet<String>();
 
     private boolean buscando = true;
     
@@ -39,85 +40,86 @@ class EstadoCalculadora {
         No no = new No(0, 0);
         
         nivelAtual.add(no);
-        
-//        No no2 = new No(0, c2);
-//        
-//        nivelAtual.add(no2);
-//        
-//        jaUsados.add(Integer.toString(c1)+",0");
-//                
-//        jaUsados.add("0,"+Integer.toString(c2));
 
-        iniciaBusca();
+        No solucao = iniciaBusca();
         
-        System.out.println("Num op: "+numeroOperacoes);
+        Stack<No> passos = new Stack<No>();
+        while(solucao != null) {
+            passos.push(solucao);
+            solucao = solucao.getPai();
+        }
+         
+        int numPassos = passos.size();
+        passos.pop();
+        System.out.println(numeroOperacoes+" acoes:");
+        for(int i = 0; i < numPassos-1; ++i) {
+            System.out.print(i+1+") ");passos.pop().mostrarMensagem();
+        }
     }
     
-    public void iniciaBusca () {
-        //3
-//15
-//4
+    public No iniciaBusca () {
         int resultado = 0, balde1 = 0, balde2 = 0;
         int vetorAuxiliar[] = new int[2];
         int baldes[] = new int[2];
+        
         while (buscando) {
+            ArrayList<No> filhos = new ArrayList<No>();
             
-//            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ começando a avaliacao do seguinte nivelAtual: ************");
-//            for(No nivelAtual : nivelAtual) {
-//                System.out.println("balde1 do nivel atual "+nivelAtual.getValor()[0]);
-//                System.out.println("balde2 do nivel atua "+nivelAtual.getValor()[1]);
-//            }
-            
-            ArrayList<No> filhos = new ArrayList<>();
+            String str = null;
 
             //iterando pelos nós do nível atual
             for(No nivelAtual : nivelAtual) {
                 //iterando pelas acoes que podem ser feitas com os baldes
-//                System.out.println("******************** to no nó "+nivelAtual.getValor()[0]+" e "+nivelAtual.getValor()[1]+" ********************");
-                
                 for (int i = 0; i < 6; i++) {
                     switch (i) {
                         case 0:
+                            str = "Encher o recipiente 0;";
+                            
                             if (balde1 != this.c1) {
                                 balde1 = encheC1();
                                 balde2 = nivelAtual.getValor()[1];
-//                                System.out.println("\nenchendo c1\n");
                             } else {
                                 balde1 = nivelAtual.getValor()[0];
                                 balde2 = nivelAtual.getValor()[1];
                             }
                             break;
                         case 1:
+                            str = "Encher o recipiente 1;";
+
                             if (balde2 != this.c2) {
                                 balde2 = encheC2();
                                 balde1 = nivelAtual.getValor()[0];
-//                                System.out.println("\nenchendo c2\n");
                             }   else {
                                 balde1 = nivelAtual.getValor()[0];
                                 balde2 = nivelAtual.getValor()[1];
                             }
                             break;
                         case 2:
+                            str = "Transferir do recipiente 0 para o recipiente 1;";
+                             
                             vetorAuxiliar = transfereConteudoC1C2(nivelAtual.getValor()[0], nivelAtual.getValor()[1]);
                             balde1 = vetorAuxiliar[0];
                             balde2 = vetorAuxiliar[1];
                             break;
                         case 3:
+                            str = "Transferir do recipiente 1 para o recipiente 0;";
+                            
                             vetorAuxiliar = transfereConteudoC2C1(nivelAtual.getValor()[0], nivelAtual.getValor()[1]);
                             balde1 = vetorAuxiliar[0];
                             balde2 = vetorAuxiliar[1];
                             break;
                         case 4: 
+                             str = "Remover agua do recipiente 0;";
+                            
                             // jogando agua do balde1 fora
                             balde1 = 0;
                             balde2 = nivelAtual.getValor()[1];
-//                            System.out.println("\nesvaziando c1\n");
                             break;
                         case 5:
-                            // jogando agua do balde2 fora
+                            str = "Remover agua do recipiente 1;";
+
                             balde1 = nivelAtual.getValor()[0];
                             balde2 = 0;
-//                            System.out.println("\nesvaziando c2\n");
                             break;
 
                     }
@@ -125,46 +127,25 @@ class EstadoCalculadora {
                     baldes[1] = balde2;
                     if ((balde1 == 0) && (balde2 == objetivoC2)) {
                         buscando = false;
+                        numeroOperacoes++;
+                        No solucao = new No (balde1, balde2);
+                        solucao.setarMensagem(str);
+                        nivelAtual.addFilho(solucao);
+                        return solucao;
+                        
                     } else {
-                        //System.out.println("Adicionando os seguintes valores, se possivel: "+baldes[0]+" e "+baldes[1]);
                         if (jaUsados.add(Integer.toString(baldes[0])+","+Integer.toString(baldes[1]))) {
-                            //System.out.println("Ok, foi possivel. Adicionando "+baldes[0]+" e "+baldes[1]);
+                            
                             No result = new No (balde1, balde2);
-                            //System.out.println("Adicionando "+result.getValor()[0]+" e "+result.getValor()[1]);
+                            result.setarMensagem(str);
+                            
                             filhos.add(result);
-                            No teste = this;
-                            //System.out.println("filhos depois de eu adicionar esses baldes ditos acima");
-                            //System.out.println("size do filhos "+filhos.size());
-//                            for (int j = 0; j < filhos.size(); j++) {
-//                                System.out.println("filho "+j);
-//                                System.out.println("filhos "+filhos.get(j).getValor()[0]);
-//                                System.out.println("filhos "+filhos.get(j).getValor()[1]);
-//                            }
+                            nivelAtual.addFilho(result);
                 
                         } 
-//                        else {
-//                            System.out.println("Nao foi possivel porque ja tinha");
-//                        }
                     }
                 }
-                
-//                System.out.println("********************** filhos desse nó");
-//                for (int i = 0; i < filhos.size(); i++) {
-//                    System.out.println("filho "+i);
-//                    System.out.println("filhos "+filhos.get(i).getValor()[0]);
-//                    System.out.println("filhos "+filhos.get(i).getValor()[1]);
-//
-//                }
-                
             }
-            
-//            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ imprimindo os filhos depois de gerar todos");
-//            for (int i = 0; i < filhos.size(); i++) {
-//                System.out.println("filho "+i);
-//                System.out.println("balde1 dos filhos "+filhos.get(i).getValor()[0]);
-//                System.out.println("balde2 dos filhos "+filhos.get(i).getValor()[1]);
-//
-//            }
             //adiciono mais um passo pra chegar no resultado
             numeroOperacoes++;
             
@@ -175,9 +156,8 @@ class EstadoCalculadora {
             nivelAtual.addAll(filhos);
             
             filhos.clear();
-            
-
         }
+        return null;
     }
 
     public int encheC1 () {
@@ -189,23 +169,15 @@ class EstadoCalculadora {
     }
     
     public int[] transfereConteudoC1C2 (int balde1, int balde2) {
-//                System.out.println("\n");
-
-//        System.out.println("Antes de transferir conteudo c1 para c2. Balde 1: "+balde1+" Balde 2: "+balde2);
         
         int vetor[] = new int[2];
         int diferenca;
         
         balde2 = balde2+balde1;
-//        System.out.println("balde2 "+balde2);
                 
         balde1 = 0;
         
         diferenca =  balde2-this.c2;
-        
-//        System.out.println("diferenca "+diferenca);
-//        
-//        System.out.println("balde2 "+balde2+" this.c2 "+this.c2);
         
         if (balde2 > this.c2) {
             balde2 = balde2 - diferenca;
@@ -215,17 +187,10 @@ class EstadoCalculadora {
         vetor[0] = balde1;
         vetor[1] = balde2;
         
-//        System.out.println("Depois de transferir conteudo c1 para c2. Balde 1: "+balde1+" Balde 2: "+balde2);
-
-//                System.out.println("\n");
-
        return vetor;
     }
     
     public int[] transfereConteudoC2C1 (int balde1, int balde2) {
-//        System.out.println("\n");
-//        
-//        System.out.println("Antes de transferir conteudo c2 para c1. Balde 1: "+balde1+" Balde 2: "+balde2);
         
         int vetor[] = new int[2];
         int diferenca;
@@ -242,9 +207,6 @@ class EstadoCalculadora {
         
         vetor[0] = balde1;
         vetor[1] = balde2;
-        
-//        System.out.println("Depois de transferir conteudo c2 para c1. Balde 1: "+balde1+" Balde 2: "+balde2);
-//        System.out.println("\n");
 
        return vetor;
     }
@@ -257,6 +219,8 @@ class No {
     private int baldes[] = new int[2];
     private ArrayList<No> filhos;
     private No pai;
+    String msgAtual;
+
     
     public No (int c1, int c2) {
         this.baldes[0] = c1;
@@ -269,5 +233,24 @@ class No {
     public int[] getValor() {
         return this.baldes;
     }
+    
+    public void addFilho(No filho) {
+        No pai = this;
+        filho.pai = this;
+        this.filhos.add(filho);
+    }
+    
+    public No getPai() {
+        return pai;
+    }
+    
+    public void mostrarMensagem() {
+        System.out.println(msgAtual);
+    }
+    
+    public void setarMensagem (String msg) {
+        this.msgAtual = msg;
+    }
+    
 }  
 
